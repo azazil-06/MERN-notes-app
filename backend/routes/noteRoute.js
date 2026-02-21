77a03695dc38cdf077de28232c7c63b237f73a95
Arjun <arjunprasadsa@gmail.com>
2026-02-21 12:17:40 +0530

    12:17

---
+++ backend/routes/noteRoute.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const route = express.Router();
+
+
+
+
+
+
+export default route
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,32 @@ const app = express();
 
     //route through localhost /api/
 app.get("/api/",(req,res)=>{ // if there is a request on /api/ then return this res
-    res.send("Hello") // sends this message as response 
+    res.status(200).send("Hello 10") // sends this message as response 
 }); //when your browser requests
 
-                    //return
+
+   //create new note
+app.post("/api/", (req,res) => {
+    res.status(201).json({message:"Post Created"})
+})
+
+
+app.put("/api/:id", (req,res) => {
+    res.status(201).json({message:"Post Updated"})
+})
+
+app.delete("/api/:id", (req,res) => {
+    res.status(201).json({message:"Post Deleted"})
+})
+
+
+
+
+
+
+
+
+                    //return requests
 app.listen( 5001, () => {
     console.log("Server stared on port 5001");
 });
