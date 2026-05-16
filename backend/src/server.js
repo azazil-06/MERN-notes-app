99b9aff7c99ead16781dea33b6230d7445b70893
Arjun <arjunprasadsa@gmail.com>
2026-05-16 21:01:22 +0530

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,33 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import noteRoute from "./routes/noteRoute.js";
 import { connectDB } from "./config/database.js";
-import dotenv from "dotenv";
+import ratelimiter from "./middleware/rateLimit.js";
 
 const app = express();
 dotenv.config();
 
-//middleware
-app.use(express.json())
 
-app.use("/api/",noteRoute);  //launch noteRoute on that api
 
-connectDB();
+app.use(cors({
+    origin:"http://localhost:5173",
+}));
+//middleware  parse json body b4 response
+app.use(express.json());
+app.use(ratelimiter);
+
+
+app.use("/api/",noteRoute);  //launch noteRoute on that api,   response
+
+//connect db then start application
+connectDB().then(()=>{
                     //return requests
 app.listen( 5001, () => {
     console.log("Server stared on port 5001");
 });
 
-
+});
 
 
